c4277c567492db4ec0e27995bce42ac75f018e06
mikgrinkevich <85791543+mikgrinkevich@users.noreply.github.com>
2022-03-22 16:01:00 +0300

    main functionality is done

--- oauth/app.py
+++ oauth/app.py
@@ -18,6 +18,8 @@ import requests
 from db import init_db_command
 from user import User
 
+from datetime import date
+
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
 GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
@@ -41,21 +43,30 @@ def load_user(user_id):
     return User.get(user_id)
 
 
-@app.route("/")
-def index():
+@app.route("/about")
+def about():
     if current_user.is_authenticated:
         return (
             "<p>Hello, {}! You're logged in! Email: {}</p>"
             "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a><br>'
-            '<a class="button" href="/list/city">city</a><br>'
-            '<a class="button" href="/city/date">date</a><br>'
-            '<a class="button" href="/about">about</a><br>'
-            '<a class="button" href="/useragent">useragent</a>'.format(
+            '<img src="{}" alt="Google profile pic"></img></div>'.format(
                 current_user.name, current_user.email, current_user.profile_pic
             )
         )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return """
+    <a class="button" href="/logout">Logout</a><br>
+    <a class="button" href="/list/weather/days">Weather forecast 5 days</a><br>
+    <a class="button" href="/list/weather/particular">Weather on a particular date</a><br>
+    <a class="button" href="/about">about</a><br>
+    <a class="button" href="/useragent">useragent</a>
+    """
     else:
         return '<a class="button" href="/login">Google Login</a>'
 
@@ -131,63 +142,88 @@ def logout():
     return redirect(url_for("index"))
 
 
-@app.route("/list/city", methods=['POST', 'GET'])
+@app.route("/list/weather/days", methods=['POST', 'GET'])
 def city_weather():
-    if request.method == 'POST':
-        city = request.form['city']
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            city = request.form['city']
+        else:
+            city = 'minsk'
+        api = os.environ.get("api")
+        source = urllib.request.urlopen(
+            'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + api).read()
+        list_of_data = json.loads(source)
+        data = [
+            {
+                "date": str(list_of_data['list'][0]['dt_txt']),
+                "temp": str(list_of_data['list'][0]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            },
+            {
+                "date": str(list_of_data['list'][7]['dt_txt']),
+                "temp": str(list_of_data['list'][7]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            },
+            {
+                "date": str(list_of_data['list'][15]['dt_txt']),
+                "temp": str(list_of_data['list'][15]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            },
+            {
+                "date": str(list_of_data['list'][23]['dt_txt']),
+                "temp": str(list_of_data['list'][23]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            },
+            {
+                "date": str(list_of_data['list'][31]['dt_txt']),
+                "temp": str(list_of_data['list'][31]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            },
+            {
+                "date": str(list_of_data['list'][39]['dt_txt']),
+                "temp": str(list_of_data['list'][39]['main']['temp'] - 273),
+                "city": str(list_of_data['city']['name']),
+                "country": str(list_of_data['city']['country'])
+            }
+        ]
+        print(data)
+        return render_template('weather.html', data=data)
     else:
-        city = 'minsk'
-    api = os.environ.get("api")
-    source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + api).read()
-    list_of_data = json.loads(source)
-    data = [
-        {
-        "date": str(list_of_data['list'][0]['dt_txt']),
-        "temp": str(list_of_data['list'][0]['main']['temp']-273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        },
-        {
-        "date": str(list_of_data['list'][7]['dt_txt']),
-        "temp": str(list_of_data['list'][7]['main']['temp'] - 273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        },
-        {
-        "date": str(list_of_data['list'][15]['dt_txt']),
-        "temp": str(list_of_data['list'][15]['main']['temp'] - 273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        },
-        {
-        "date": str(list_of_data['list'][23]['dt_txt']),
-        "temp": str(list_of_data['list'][23]['main']['temp'] - 273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        },
-        {
-        "date": str(list_of_data['list'][31]['dt_txt']),
-        "temp": str(list_of_data['list'][31]['main']['temp'] - 273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        },
-        {
-        "date": str(list_of_data['list'][39]['dt_txt']),
-        "temp": str(list_of_data['list'][39]['main']['temp'] - 273),
-        "city": str(list_of_data['city']['name']),
-        "country": str(list_of_data['city']['country'])
-        }
-    ]
+        return '<a class="button" href="/login">Google Login</a>'
+
+@app.route("/list/weather/particular", methods=['POST', 'GET'])
+def weather_particular_date():
+    if request.method == 'POST':
+        date_for_api = request.form['date']
+    else:
+        date_for_api = str(date.today().strftime("%Y-%m-%d"))
+    api_date = os.environ.get("api_date")
+    weather_json = urllib.request.urlopen(
+        'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/minsk,bel/' + date_for_api + '?key=' + api_date).read()
+    list_data = json.loads(weather_json)
+    data = {
+        "address": str(list_data['resolvedAddress']),
+        "timezone": str(list_data['timezone']),
+        "datetime": str(list_data['days'][0]['datetime']),
+        "temperature": str(list_data['days'][0]['temp'] - 32) + 'C',
+        "conditions": str(list_data['days'][0]['conditions']),
+        "description": str(list_data['days'][0]['description']),
+    }
     print(data)
-    return render_template('weather.html', data=data)
+    return render_template('particular_date.html', data=data)
 
 
 @app.route("/useragent")
 def useragent():
     user_agent = request.user_agent
     return (
-            '<p>your platform is: {}</p>'
-            '<p>your browser is: {}</p>'.format(user_agent.platform, user_agent.browser)
+        '<p>your platform is: {}</p>'
+        '<p>your browser is: {}</p>'.format(user_agent.platform, user_agent.browser)
     )
 
 
